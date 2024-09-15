53ce32c46599ceebc44d04d130554f2fe1c69776
Caro <32751028-karoar@users.noreply.replit.com>
2024-09-15 04:55:21 +0000

    ejercicios-s2

--- main.py
+++ main.py
@@ -1,71 +1,90 @@
-#S2: Sentencias Basicas
-#Flujo de Control de un Programa (FCP)
-#Una secuencia de sentencias ordenadas renglon por renglon y respentando su sintaxis
-#Funciones:
-#Es codigo reutilizable que realiza una tarea especifica.
+#S2: Sentencias Basicas - Ejercicios
+#Intentá ser lo más amigable posible con el usuario!
 
-#Disponemos de una amplia gama de funciones predefinidas tales como:
-#print(),input(), abs(), round(),
-#int(), float(), str(), bool(), len()
+#Ejercicio 1
+#Crear un programa que le solicite al usuario un entero y lo imprima por pantalla.
 
-#Para definir funciones propias utilizamos el comando 'def'
+numero = input('Ingrese un numero del 1 al 4:')
+print ('Eligio el numero:' + numero)
 
-def saludar(nombre): #definir funcion
-    print(f"Hola, {nombre}")
+#Ejercicio 2
+#Crear un programa que le solicite al usuario dos números enteros y luego imprima por pantalla:
 
-saludar("Caro") #Llama a la función y muestra "Hola, 'nombre'"
+num1 = int(input('Ingrese un numero:'))
+num2 = int(input('Ingrese otro numero:'))
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+resultado = suma,resta,multiplicacion,division
+print(resultado)
 
-#Diferencia entre RETURN y PRINT
-#return devuelve un valor y termina la ejecucion de la funcion
-#print solo muestra el valor y no devuelve nada
+#Ejercicio 3
+#Crear un programa que le solicite al usuario un entero y determine si es par
+#Mostrando por pantalla un mensaje que indique el resultado
 
+numero = int(input("Ingresa un número entero: "))
+resultado = "par" if numero % 2 == 0 else "impar"
+print(f"El número {numero} es {resultado}.")
 
-#Variables
-#Es el nombre que se asocia a un valor variable
-#a lo largo de la ejecucion del programa
-#edad = 25 variable que almacena un numero
-#se escriben en uppercase
+#Ejecicio 4
+#Escribir un programa que le pida a un usuario su año de nacimiento y otro año
+#Y le diga qué edad tenía el usuario en el año ingresado.
 
+anioNac = int(input("Ingrese su año de nacimiento:"))
+anioExt = int(input("Ingrese otro año:"))
+edad = anioExt - anioNac
+print("En el año", anioExt , "tenia la edad de ", edad)
 
-#Constantes 
-#Es un valor no variable durante la ejecucion
-#PI = 3.1416 normalmente se escribe en mayusculas
+#Ejercio 5
+#Crear un programa que le solicite al usuario 5 enteros
+#Y muestre por pantalla el promedio de ellos
 
-#Tipos de Datos
-# entero - int = 9
-#real - float = 0.01
-#complejo - complex =1+2j
-#logico - bool = True / False
-#texto - str = "Hola"
+nums1 = int(input("Ingrese un numeros entero:"))
+nums2 = int(input("Ingrese un numeros entero:"))
+nums3 = int(input("Ingrese un numeros entero:"))
+nums4 = int(input("Ingrese un numeros entero:"))
+nums5 = int(input("Ingrese un numeros entero:"))
+resultado = (nums1 + nums2 + nums3 + nums4 + nums5) / 5
+print(resultado)
 
-#Texto,String o Cadena (str) referencia la secuencia de caracteres sean letras,numeros
-# o simbolos
-#Se usa para almacenar y manipular texto
-#Cadenas de caracteres ordenados cuya posicion
-#inicia en 0,de izquierda a derecha
+#Ejercio 6
+#Crear una función que reciba un número 
+#Y muestre el anterior y el siguiente.
 
-palabra="pensamiento"
-print(palabra[2])
+numIng = input("Ingrese un numero:")
+numAnt = int(numIng)-1
+numSig = int(numIng)+1
+print("El numero ingresado es", numIng, "el numero anterior es", numAnt , "y el numero siguiente es ", numSig)
 
-#Rangos
-#Start desde donde inicia la cadena (0)
-#End hasta donde se define que llegue excluyendo la posicion ([5] llega a 4ta posicion)
-#Setp cada cuanto se va aumentando la posicion(1)
+#Ejercio 7
+#Crear una función que una un string y un entero, ambos dentro de un string
 
-#[start:end:step]
-print(palabra[1:6:2])
+nombre = input("Ingrese su nombre:")
+edad = int(input("Ingrese su edad:"))
+print(f"Hola", {nombre},"tienes",{edad})
 
-#Si un valor no es asignado se queda el valor predeterminado
-#Start 0
-#End -1
-#Step 1
-print(palabra[:6:2])
-print(palabra[1::2])
-print(palabra[1:6:])
+#Ejercio 8 
+#Pedirle nombre y apellido por separado e imprimir “Apellido, Nombre”
 
-#Input 
-#Es una funcion que permite obtener la entrada de un user
+nomb = input("Ingrese su nombre:")
+apell = input("Ingrese su apellido:")
+print("Su nombre completo es:", nomb ,"", apell)
 
-materia=input('Complete el nombre de la materia: ')
-print(palabra+ " " + materia )
+#Ejecicio 9 
+#Obtener una palabra e imprimir la cantidad de letras
+palabra = input("Ingrese una palabra:")
+cantPal = len(palabra)
+print("Su palabra contiene",cantPal, "caracteres")
 
+#Ejercicio 10
+#Obtener una palabra e imprimir los primeros 5 caracteres
+
+palabra = input("Ingrese una palabra:")
+print(palabra[:5:])
+
+#Ejercicio 11
+#Obtener una palabra, borrarle todas las ‘a’ e imprimirla por pantalla
+palabra = input("Ingrese una palabra que contenga la letra a:")
+palabra_sin_a = palabra.replace('a', '').replace('A', '')
+print(palabra_sin_a)
