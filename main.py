86a816fcc34184dc1d6f3c2d8bd9e3c2576d3a40
Caro <32751028-karoar@users.noreply.replit.com>
2024-09-15 02:27:58 +0000

    conceptosbasicos

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#
+#S1: Conceptos Basicos
+#Un Programa de Computadora es un Algoritmo escrito en un Lenguaje de Programación.
+#Un programa (P) es la suma de algoritmo (A) y datos (D). P+A=D 
+#Dato es toda la informacion o dato que se necesite,es un poco ambiguo el significado
+#Algoritmo es una serie finita de pasos que se necesita para un proceso
+
+print ("Hola Mundo")
+#Python actua como interprete,transldando los pasos el algoritmo o programa en un formato
+#que la computadora pueda entenderlo y ejecutarlo
